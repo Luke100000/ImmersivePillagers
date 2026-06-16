9ddc0b26ad87bd9fc024b892574834002a31be3d
Luke100000 <jonas.peche@aon.at>
2026-06-16 11:00:57 +0200

    cleanup

--- forge/src/main/java/net/conczin/immersive_pillagers/forge/ImmersivePillagersForge.java
+++ forge/src/main/java/net/conczin/immersive_pillagers/forge/ImmersivePillagersForge.java
@@ -2,7 +2,6 @@ package net.conczin.immersive_pillagers.forge;
 
 import net.conczin.immersive_pillagers.ImmersivePillagers;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(ImmersivePillagers.MOD_ID)
 public class ImmersivePillagersForge {
