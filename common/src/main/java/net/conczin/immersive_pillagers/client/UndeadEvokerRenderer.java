a1ad159f1572cb2cee20474d44856b7a5c98b61c
Luke100000 <jonas.peche@aon.at>
2026-08-13 21:52:59 +0200

    fixes

--- common/src/main/java/net/conczin/immersive_pillagers/client/UndeadEvokerRenderer.java
+++ common/src/main/java/net/conczin/immersive_pillagers/client/UndeadEvokerRenderer.java
@@ -3,7 +3,6 @@ package net.conczin.immersive_pillagers.client;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.conczin.immersive_pillagers.ImmersivePillagers;
 import net.conczin.immersive_pillagers.entity.UndeadEvoker;
-import net.minecraft.client.model.IllagerModel;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.IllagerRenderer;
@@ -15,7 +14,7 @@ public class UndeadEvokerRenderer extends IllagerRenderer<UndeadEvoker> {
 
     public UndeadEvokerRenderer(EntityRendererProvider.Context context) {
         super(context, new UndeadIllagerModel<>(context.bakeLayer(UndeadModelLayers.UNDEAD_ILLAGER)), 0.5f);
-        this.addLayer(new ItemInHandLayer<UndeadEvoker, IllagerModel<UndeadEvoker>>(this, context.getItemInHandRenderer()) {
+        this.addLayer(new ItemInHandLayer<>(this, context.getItemInHandRenderer()) {
             @Override
             public void render(PoseStack poseStack, MultiBufferSource multiBufferSource, int i, UndeadEvoker entity, float f, float g, float h, float j, float k, float l) {
                 if (entity.isCastingSpell()) {
--- common/src/main/java/net/conczin/immersive_pillagers/client/UndeadVindicatorRenderer.java
+++ common/src/main/java/net/conczin/immersive_pillagers/client/UndeadVindicatorRenderer.java
@@ -1,7 +1,9 @@
 package net.conczin.immersive_pillagers.client;
 
+import com.mojang.blaze3d.vertex.PoseStack;
 import net.conczin.immersive_pillagers.ImmersivePillagers;
 import net.conczin.immersive_pillagers.entity.UndeadVindicator;
+import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.IllagerRenderer;
 import net.minecraft.client.renderer.entity.layers.ItemInHandLayer;
@@ -12,7 +14,14 @@ public class UndeadVindicatorRenderer extends IllagerRenderer<UndeadVindicator> 
 
     public UndeadVindicatorRenderer(EntityRendererProvider.Context context) {
         super(context, new UndeadIllagerModel<>(context.bakeLayer(UndeadModelLayers.UNDEAD_ILLAGER)), 0.5f);
-        this.addLayer(new ItemInHandLayer<>(this, context.getItemInHandRenderer()));
+        this.addLayer(new ItemInHandLayer<>(this, context.getItemInHandRenderer()) {
+            @Override
+            public void render(PoseStack poseStack, MultiBufferSource multiBufferSource, int i, UndeadVindicator entity, float f, float g, float h, float j, float k, float l) {
+                if (entity.isAggressive()) {
+                    super.render(poseStack, multiBufferSource, i, entity, f, g, h, j, k, l);
+                }
+            }
+        });
     }
 
     @Override
