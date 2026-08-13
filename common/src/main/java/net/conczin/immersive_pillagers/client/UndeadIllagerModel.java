53266095e1e5fde0285570d0da8fc5cd4067a1a8
Luke100000 <jonas.peche@aon.at>
2026-08-13 21:43:31 +0200

    textures and models

--- common/src/main/java/net/conczin/immersive_pillagers/client/UndeadIllagerModel.java
+++ common/src/main/java/net/conczin/immersive_pillagers/client/UndeadIllagerModel.java
@@ -18,26 +18,32 @@ public class UndeadIllagerModel<T extends AbstractIllager> extends IllagerModel<
         head.addOrReplaceChild("hat", CubeListBuilder.create().texOffs(32, 0).addBox(-4.0f, -10.0f, -4.0f, 8.0f, 12.0f, 8.0f, new CubeDeformation(0.45f)), PartPose.ZERO);
         head.addOrReplaceChild("nose", CubeListBuilder.create().texOffs(24, 0).addBox(-1.0f, -1.0f, -6.0f, 2.0f, 4.0f, 2.0f), PartPose.offset(0.0f, -2.0f, 0.0f));
         partDefinition.addOrReplaceChild("body", CubeListBuilder.create().texOffs(16, 20).addBox(-4.0f, 0.0f, -3.0f, 8.0f, 12.0f, 6.0f).texOffs(0, 38).addBox(-4.0f, 0.0f, -3.0f, 8.0f, 20.0f, 6.0f, new CubeDeformation(0.5f)), PartPose.offset(0.0f, 0.0f, 0.0f));
-        PartDefinition arms = partDefinition.addOrReplaceChild("arms", CubeListBuilder.create().texOffs(44, 22).addBox(-8.0f, -2.0f, -2.0f, 4.0f, 8.0f, 4.0f).texOffs(40, 38).addBox(-4.0f, 2.0f, -2.0f, 8.0f, 4.0f, 4.0f), PartPose.offsetAndRotation(0.0f, 3.0f, -1.0f, -0.75f, 0.0f, 0.0f));
+        PartDefinition arms = partDefinition.addOrReplaceChild("arms", CubeListBuilder.create()
+                .texOffs(44, 22).addBox(-8.0f, -2.0f, -2.0f, 4.0f, 8.0f, 4.0f)
+                .texOffs(40, 38).addBox(-4.0f, 2.0f, -2.0f, 8.0f, 4.0f, 4.0f),
+                PartPose.offsetAndRotation(0.0f, 3.0f, -1.0f, -0.75f, 0.0f, 0.0f));
+        arms.addOrReplaceChild("folded_arm_bone", CubeListBuilder.create()
+                .texOffs(44, 0).addBox(-0.75f, -6.0f, -1.0f, 1.5f, 12.0f, 2.0f),
+                PartPose.offsetAndRotation(0.0f, 4.0f, 0.0f, 0.0f, 0.0f, (float)Math.PI/2f));
         arms.addOrReplaceChild("left_shoulder", CubeListBuilder.create().texOffs(44, 22).mirror().addBox(4.0f, -2.0f, -2.0f, 4.0f, 8.0f, 4.0f), PartPose.ZERO);
         partDefinition.addOrReplaceChild("right_leg", CubeListBuilder.create()
                         .texOffs(0, 22).addBox(-2.0f, 0.0f, -2.0f, 4.0f, 12.0f, 4.0f)
-                        .texOffs(56, 0).addBox(-1.0f, 0.0f, -1.0f, 2.0f, 12.0f, 2.0f),
+                        .texOffs(46, 24).addBox(-0.75f, 2.0f, -1.0f, 1.5f, 12.0f, 2.0f),
                 PartPose.offset(-2.0f, 12.0f, 0.0f));
 
         partDefinition.addOrReplaceChild("left_leg", CubeListBuilder.create()
                         .texOffs(0, 22).mirror().addBox(-2.0f, 0.0f, -2.0f, 4.0f, 12.0f, 4.0f)
-                        .texOffs(56, 0).addBox(-1.0f, 0.0f, -1.0f, 2.0f, 12.0f, 2.0f),
+                        .texOffs(46, 24).addBox(-0.75f, 2.0f, -1.0f, 1.5f, 12.0f, 2.0f),
                 PartPose.offset(2.0f, 12.0f, 0.0f));
 
         partDefinition.addOrReplaceChild("right_arm", CubeListBuilder.create()
                         .texOffs(40, 46).addBox(-3.0f, -2.0f, -2.0f, 4.0f, 12.0f, 4.0f)
-                        .texOffs(34, 0).addBox(-2.0f, -2.0f, -1.0f, 2.0f, 12.0f, 2.0f),
+                        .texOffs(34, 0).addBox(-1.75f, 0.0f, -1.0f, 1.5f, 12.0f, 2.0f),
                 PartPose.offset(-5.0f, 2.0f, 0.0f));
 
         partDefinition.addOrReplaceChild("left_arm", CubeListBuilder.create()
                         .texOffs(40, 46).mirror().addBox(-1.0f, -2.0f, -2.0f, 4.0f, 12.0f, 4.0f)
-                        .texOffs(44, 0).addBox(0.0f, -2.0f, -1.0f, 2.0f, 12.0f, 2.0f),
+                        .texOffs(44, 0).addBox(0.25f, 0.0f, -1.0f, 1.5f, 12.0f, 2.0f),
                 PartPose.offset(5.0f, 2.0f, 0.0f));
 
         return LayerDefinition.create(meshDefinition, 64, 64);
---
+++ common/src/main/java/net/conczin/immersive_pillagers/entity/UndeadVindicator.java
@@ -0,0 +1,46 @@
+package net.conczin.immersive_pillagers.entity;
+
+import net.conczin.immersive_pillagers.ImmersivePillagersSounds;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobType;
+import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.entity.monster.Vindicator;
+import net.minecraft.world.level.Level;
+
+public class UndeadVindicator extends Vindicator {
+    public UndeadVindicator(EntityType<? extends Vindicator> entityType, Level level) {
+        super(entityType, level);
+    }
+
+    @Override
+    public MobType getMobType() {
+        return MobType.UNDEAD;
+    }
+
+    public static AttributeSupplier.Builder createAttributes() {
+        return Monster.createMonsterAttributes()
+                .add(Attributes.MOVEMENT_SPEED, 0.28D)
+                .add(Attributes.MAX_HEALTH, 30.0D)
+                .add(Attributes.FOLLOW_RANGE, 32.0D)
+                .add(Attributes.ATTACK_DAMAGE, 5.0D);
+    }
+
+    @Override
+    protected SoundEvent getAmbientSound() {
+        return ImmersivePillagersSounds.UNDEAD_VINDICATOR_AMBIENT;
+    }
+
+    @Override
+    protected SoundEvent getHurtSound(DamageSource damageSource) {
+        return ImmersivePillagersSounds.UNDEAD_VINDICATOR_HURT;
+    }
+
+    @Override
+    protected SoundEvent getDeathSound() {
+        return ImmersivePillagersSounds.UNDEAD_VINDICATOR_DEATH;
+    }
+}
